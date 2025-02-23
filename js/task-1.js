e2f40b9ef5ef2fd019a7bddcd0e1133c3a240d27
VladyslavIvanenko <v.ivanenko.optimus@gmail.com>
2025-02-23 21:10:06 +0200

    done

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,15 @@
-function makeTransaction(quantity, pricePerOrder) {
-  return `You ordered ${quantity} droids worth ${
-    quantity * pricePerOrder
-  } credits!`;
+function isEnoughCapacity(products, containerSize) {
+  const totalProducts = Object.values(products).reduce(
+    (sum, quantity) => sum + quantity,
+    0
+  );
+
+  return totalProducts <= containerSize;
 }
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
